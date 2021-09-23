b26c29ed04af5624e9b8d6347eac3cfe53b8dc4f
bytesx <pnada.black.edg@gmail.com>
2021-09-23 18:52:55 -0500

    Comenzando Java con Arreglos

---
+++ src/Arreglos1.java
@@ -0,0 +1,14 @@
+public class Arreglos1 {
+    public static void main(String[] args) {
+
+        // Arreglos - Java - Video1
+
+        String nombres [];
+        nombres = new String[3];
+
+
+        System.out.println(nombres[0]);
+        System.out.println(nombres[1]);
+        System.out.println(nombres[2]);
+    }
+}
